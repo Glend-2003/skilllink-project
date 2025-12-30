87d72b8aec0eaa23f855eeb7b426913c459b8904
Jamel Sandí <jsandi12199@gmail.com>
2025-12-29 23:03:02 -0600

    feat(service-manager): init project structure and categories CRUD
    - Initialized NestJS modular architecture.
    - Configured MySQL connection with TypeORM.
    - Implemented Service Categories module (Entity, DTOs, Service, Controller).
    - Added global validation pipes (class-validator).
    - Fixed .gitignore to exclude node_modules.

--- node_modules/service-manager/src/app.module.ts
+++ node_modules/service-manager/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// 1. IMPORTANTE: Traemos el módulo de categorías
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'skilllink_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    CategoriesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
