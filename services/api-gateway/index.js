011bd0518cabcf87339cf4d6dffa30622937c4c1
deyCedeno <162640178+deyCedeno@users.noreply.github.com>
2026-01-27 22:34:18 -0600

    Update the api-gateway endpoints
    Payment-services and services manager were not configured in the api-gateway index.js

--- services/api-gateway/index.js
+++ services/api-gateway/index.js
@@ -20,6 +20,28 @@ app.use('/api/v1/services', createProxyMiddleware({
     changeOrigin: true
 }));
 
+
+// providers.controller.ts -> @Controller('providers')
+app.use('/api/v1/providers', createProxyMiddleware({
+    target: 'http://localhost:3002/providers',
+    changeOrigin: true,
+    pathRewrite: { '^/api/v1/providers': '' }
+}));
+
+// categories.controller.ts -> @Controller('categories')
+app.use('/api/v1/categories', createProxyMiddleware({
+    target: 'http://localhost:3002/categories',
+    changeOrigin: true,
+    pathRewrite: { '^/api/v1/categories': '' }
+}));
+
+// gallery.controller.ts -> @Controller('gallery')
+app.use('/api/v1/gallery', createProxyMiddleware({
+    target: 'http://localhost:3002/gallery',
+    changeOrigin: true,
+    pathRewrite: { '^/api/v1/gallery': '' }
+}));
+
 // Chat (Node + WebSocket) - Puerto 3003
 app.use('/api/v1/chat', createProxyMiddleware({
     target: 'http://localhost:3003',
@@ -38,6 +60,13 @@ app.use('/api/v1/reviews', createProxyMiddleware({
 app.use('/api/v1/users', createProxyMiddleware({
     target: 'http://localhost:3004',
     changeOrigin: true
+}));
+
+// Payment-Service (Spring Boot - Java) - 3005
+app.use('/api/v1/payments', createProxyMiddleware({
+    target: 'http://localhost:3005',
+    changeOrigin: true,
+    pathRewrite: { '^/api/v1/payments': '/api/payments' }
 }));
 
 app.listen(3000, () => console.log('🚀 API Gateway on port 3000'));
